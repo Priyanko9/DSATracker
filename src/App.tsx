66b35a2b013f45c51b0c00beb14ab33b4ed5a0eb
Priyanko Das <pridas2@wkmin2740568.global.publicisgroupe.net>
2021-03-06 23:55:18 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect} from 'react';
+import xlsx from 'xlsx';
 import './App.css';
 
 function App() {
+
+  useEffect(()=>{
+    let fileReader=new FileReader();
+    // fetch('./FINAL450.xlsx')
+    //   .then((response)=>response.blob())
+    //   .then((blob)=>fileReader.readAsBinaryString(blob))
+    //   .
+    
+    const readExcel=async () => {
+      
+      let file=await fetch('./FINAL450.xlsx');
+      let fileBuffer=await file.arrayBuffer();
+      let data = new Uint8Array(fileBuffer);
+      // fileReader.readAsArrayBuffer(data);
+      let workbook=xlsx.read(data,{type:"array"});
+      console.log(workbook);
+      workbook.SheetNames.forEach((sheet)=>{
+        const jsonData=xlsx.utils.sheet_to_json(workbook.Sheets[sheet] , { raw: true });
+        console.log("jsonData:",jsonData);
+      })
+      // fileReader.readAsBinaryString(fileBuffer)
+      
+    }
+    // fileReader.onload=(event:ProgressEvent<FileReader>)=>{
+    //   let data=event.target!.result;
+    //   console.log("data:",data);
+    //   let workbook=xlsx.read(data,{type:"array"});
+    //   console.log(workbook);
+ 
+    // }
+    readExcel();
+  },[])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
